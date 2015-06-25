9e3d1dd7ea8677329765861ab03d1ae4de1e3cd6
Markus Felten <Markus.Felten@gmx.de>
2015-06-26 00:52:11 +0200

    flow steps as array

--- app/routes/flow.js
+++ app/routes/flow.js
@@ -5,6 +5,14 @@ export default Ember.Route.extend({
   model: function (params) {
     return fetch(`flows/${params.flow_id}`).then((response) => {
       return response.json().then((data) => {
+
+        const steps = [];
+
+        for(let s in data.steps) {
+          steps.push(data.steps[s]);
+        }
+        data.steps = steps;
+
         console.log(`response: ${JSON.stringify(data)}`);
         return data;
       });
