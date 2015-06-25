f7dff1567f9e6481232fdcf743c5960b8fe5b853
Markus Felten <Markus.Felten@gmx.de>
2015-06-25 20:28:14 +0200

    simple flow fetching

--- app/routes/flows.js
+++ app/routes/flows.js
@@ -1,15 +1,36 @@
 import Ember from 'ember';
+import fetch from 'fetch';
 
 export default Ember.Route.extend({
   model: function () {
-    return [{
-      id: "flow1_id",
-      name: "flow1",
-      description: "description of flow1"
-    }, {
-      id: "flow2_id",
-      name: "flow2",
-      description: "description of flow2"
-    }];
+    return fetch('/flows').then(function (response) {
+      return response.json().then(function (data) {
+        //console.log(JSON.stringify(data));
+
+        const flows = data.map(function (f) {
+          return {
+            id: f,
+            name: f,
+            description: `description ${f}`
+          };
+        });
+
+        //console.log(JSON.stringify(flows));
+
+        return flows;
+      });
+    });
+
+    /*
+        return [{
+          id: "flow1_id",
+          name: "flow1",
+          description: "description of flow1"
+        }, {
+          id: "flow2_id",
+          name: "flow2",
+          description: "description of flow2"
+        }];
+        */
   }
 });
