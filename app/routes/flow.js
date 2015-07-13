30eed043d21fbfdb5cf3bd0634394a048f9847e5
Markus Felten <Markus.Felten@gmx.de>
2015-07-13 23:54:11 +0200

    playing around with svg layout

--- app/routes/flow.js
+++ app/routes/flow.js
@@ -8,25 +8,35 @@ export default Ember.Route.extend({
 
         const steps = [];
 
-        let y = 0;
+        let y = 150;
+        let x = 150;
 
         for(let s in data.steps) {
           const step = data.steps[s];
           const endpoints = [];
 
-          step.x = 0;
+          step.x = x;
           step.y = y;
-          y += 100;
+          step.w = 80;
+          step.h = 50;
+          step.tx = x + 30;
+          step.ty = y + 30;
 
+          let n = 0;
           for(let e in step.endpoints) {
             const ep = step.endpoints[e];
             ep.isIn = ep.direction.match(/in/) ? true : false;
             ep.isOut = ep.direction.match(/out/) ? true : false;
 
+            ep.x = x + 10;
+            ep.y = y + 20 + n * 15;
+            n++;
             endpoints.push(ep);
           }
           step.endpoints = endpoints;
           steps.push(step);
+
+          y += 60;
         }
         data.steps = steps;
 
