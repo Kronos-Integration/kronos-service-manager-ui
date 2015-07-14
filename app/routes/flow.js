9d07d16f3536ed19ef8ffda7aba4dff6801699a5
Markus Felten <Markus.Felten@gmx.de>
2015-07-14 23:27:21 +0200

    svg/css work

--- app/routes/flow.js
+++ app/routes/flow.js
@@ -6,6 +6,19 @@ export default Ember.Route.extend({
     return fetch(`flows/${params.flow_id}`).then((response) => {
       return response.json().then((data) => {
 
+        for(let s in data.steps) {
+          const step = data.steps[s];
+          for(let e in step.endpoints) {
+            const ep = step.endpoints[e];
+            const m = ep.target.match(/^step:([^/]+)\/(.+)/);
+            if(m) {
+              const targetStep = m[1];
+              const targetEndpoint = m[2];
+              ep.counterpart = data.steps[targetStep].endpoints[targetEndpoint];
+            }
+          }
+        }
+
         const steps = [];
 
         let y = 150;
@@ -28,7 +41,7 @@ export default Ember.Route.extend({
             ep.isIn = ep.direction.match(/in/) ? true : false;
             ep.isOut = ep.direction.match(/out/) ? true : false;
 
-            ep.x = x + 10;
+            ep.x = x + (ep.isOut ? 70 : 10);
             ep.y = y + 20 + n * 15;
             n++;
             endpoints.push(ep);
@@ -37,10 +50,14 @@ export default Ember.Route.extend({
           steps.push(step);
 
           y += 60;
+
+          if(y > 250) {
+            x += 150;
+            y = 150;
+          }
         }
         data.steps = steps;
 
-        //console.log(`response: ${JSON.stringify(data)}`);
         return data;
       });
     });
