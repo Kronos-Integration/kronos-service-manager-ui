93d0ce23cedd7769d442a773658e8d9420d3ede4
Markus Felten <Markus.Felten@gmx.de>
2015-07-19 20:18:48 +0200

    dummy websocket support added

--- app/controllers/flow.js
+++ app/controllers/flow.js
@@ -1,6 +1,25 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+
+  socketService: Ember.inject.service('websockets'),
+  init: function() {
+    this._super.apply(this, arguments);
+
+    var socket = this.get('socketService').socketFor('ws://localhost:4200/flows');
+
+    socket.on('open', function(event) {
+      console.log('open');
+      socket.send('Hello Websocket World');
+    }, this);
+    socket.on('message', function(event) {
+      console.log('message');
+    }, this);
+    socket.on('close', function(event) {
+      console.log('close');
+    }, this);
+  },
+
   actions: {
     drag: function(step) {
       //step.x = 100;
