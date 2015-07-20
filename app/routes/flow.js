f8afadf340c5a41760e4be53b46cb6287447e406
Markus Felten <Markus.Felten@gmx.de>
2015-07-21 00:50:51 +0200

    feat(flow): added step label

--- app/routes/flow.js
+++ app/routes/flow.js
@@ -6,12 +6,12 @@ export default Ember.Route.extend({
     return fetch(`flows/${params.flow_id}`).then((response) => {
       return response.json().then((data) => {
 
-        for(let s in data.steps) {
+        for (let s in data.steps) {
           const step = data.steps[s];
-          for(let e in step.endpoints) {
+          for (let e in step.endpoints) {
             const ep = step.endpoints[e];
             const m = ep.target.match(/^step:([^/]+)\/(.+)/);
-            if(m) {
+            if (m) {
               const targetStep = m[1];
               const targetEndpoint = m[2];
               ep.counterpart = data.steps[targetStep].endpoints[targetEndpoint];
@@ -34,28 +34,30 @@ export default Ember.Route.extend({
         let x = originX;
         let y = originY;
 
-        for(let s in data.steps) {
+        for (let s in data.steps) {
           const step = data.steps[s];
           const endpoints = [];
 
           step.x = x;
           step.y = y;
           step.w = stepW;
+          step.nx = x + stepW / 2;
           step.tx = x + stepW / 2;
 
-          let nIn = 0, nOut = 0;
+          let nIn = 0,
+            nOut = 0;
 
-          for(let e in step.endpoints) {
+          for (let e in step.endpoints) {
             const ep = step.endpoints[e];
 
             ep.isIn = ep.direction.match(/in/) ? true : false;
-            if(ep.isIn) {
+            if (ep.isIn) {
               ep.x = x + endpointOffset;
               ep.y = y + endpointOffset + nIn++ * endpointOffset;
             }
 
             ep.isOut = ep.direction.match(/out/) ? true : false;
-            if(ep.isOut) {
+            if (ep.isOut) {
               ep.x = x + stepW - endpointOffset;
               ep.y = y + endpointOffset + nOut++ * endpointOffset;
             }
@@ -64,14 +66,15 @@ export default Ember.Route.extend({
           }
 
           step.h = ((nIn > nOut ? nIn : nOut) + 1) * endpointOffset;
-          step.ty = y + step.h / 2 + 5;
+          step.ny = y + step.h / 2 + 5;
+          step.ty = y + 10;
 
           step.endpoints = endpoints;
           steps.push(step);
 
           y += step.h + stepOffset;
 
-          if(y > originY + 4 * stepH) {
+          if (y > originY + 4 * stepH) {
             x += stepW + stepOffset;
             y = originY;
           }
