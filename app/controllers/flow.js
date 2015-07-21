ddfa8db22a9ed07b81c91bfb3bf6d8b9eebcf5ad
Markus Felten <Markus.Felten@gmx.de>
2015-07-21 21:32:45 +0200

    feat: calculate ws url from window location

--- app/controllers/flow.js
+++ app/controllers/flow.js
@@ -6,14 +6,15 @@ export default Ember.Controller.extend({
   init: function() {
     this._super.apply(this, arguments);
 
-    var socket = this.get('socketService').socketFor('ws://localhost:4200/flows');
+    const location = `ws://${window.location.host}/flows`;
+    const socket = this.get('socketService').socketFor(location);
 
     socket.on('open', function(event) {
       console.log('open');
       socket.send('Hello Websocket World');
     }, this);
     socket.on('message', function(event) {
-      console.log('message');
+      console.log('message: ' + event.data)
     }, this);
     socket.on('close', function(event) {
       console.log('close');
