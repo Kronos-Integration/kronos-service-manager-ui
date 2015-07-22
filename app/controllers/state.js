bf15b6b0933cec61ea2703089fa8ec3a124a09d4
Markus Felten <Markus.Felten@gmx.de>
2015-07-23 00:53:32 +0200

    feat(state): consume state updates over websocket - not reflected in model for now

--- app/controllers/state.js
+++ app/controllers/state.js
@@ -2,31 +2,25 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
 
-/*
   socketService: Ember.inject.service('websockets'),
   init: function() {
     this._super.apply(this, arguments);
 
-    const location = `ws://${window.location.host}/flow`;
+    const location = `ws://${window.location.host}/state`;
     const socket = this.get('socketService').socketFor(location);
 
-    socket.on('open', function(event) {
-      console.log('open');
-      socket.send('Hello Websocket World');
-    }, this);
     socket.on('message', function(event) {
-      console.log('message: ' + event.data)
+      const data = JSON.parse(event.data);
+      console.log(`uptime: ${data.uptime}`);
+
+      this.set('uptime',data.uptime);
+      /*
+      this.set('memory.heapTotal',data.memory.heapTotal);
+      this.set('memory.heapUsed',data.memory.heapUsed);
+      */
     }, this);
     socket.on('close', function(event) {
       console.log('close');
     }, this);
-  },
-*/
-  actions: {
-    drag: function(step) {
-      //step.x = 100;
-      //step.set('x',step.get('x') + 10);
-      console.log(`drag: ${step.name}`);
-    }
   }
 });
