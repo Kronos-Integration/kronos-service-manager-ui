7276c63781e192ce2b3dff90983f119ba20234fb
Markus Felten <Markus.Felten@gmx.de>
2015-07-25 10:59:01 +0200

    refactor: code cleanup only

--- app/controllers/state.js
+++ app/controllers/state.js
@@ -3,23 +3,23 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
 
   socketService: Ember.inject.service('websockets'),
-  init: function() {
+  init: function () {
     this._super.apply(this, arguments);
 
     const location = `ws://${window.location.host}/state`;
     const socket = this.get('socketService').socketFor(location);
 
-    socket.on('message', function(event) {
+    socket.on('message', function (event) {
       const data = JSON.parse(event.data);
       console.log(`uptime: ${data.uptime}`);
 
-      this.set('uptime',data.uptime);
+      this.set('uptime', data.uptime);
       /*
       this.set('memory.heapTotal',data.memory.heapTotal);
       this.set('memory.heapUsed',data.memory.heapUsed);
       */
     }, this);
-    socket.on('close', function(event) {
+    socket.on('close', function () {
       console.log('close');
     }, this);
   }
