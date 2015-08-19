22f7dd9471719c7e638dac827b688645f285f2f0
Markus Felten <Markus.Felten@gmx.de>
2015-08-19 19:39:57 +0200

    feat: (not working) trying to reopen socket if connection gets lost

--- app/controllers/state.js
+++ app/controllers/state.js
@@ -7,13 +7,22 @@ export default Ember.Controller.extend({
     this._super.apply(this, arguments);
 
     const location = `ws://${window.location.host}/state`;
-    const socket = this.get('socketService').socketFor(location);
+    let socket = this.get('socketService').socketFor(location);
+    let intervalHandler;
+    const myself = this;
 
     socket.on('open', function() {
+      console.log(`(Re)open socket ${location}`);
       this.set('content.connected',true);
+      clearInterval(intervalHandler);
       }, this);
     socket.on('close', function () {
+      socket = undefined;
       this.set('content.connected',false);
+      intervalHandler = setInterval(function() {
+        socket = myself.get('socketService').socketFor(location);
+        console.log(`Trying to reopen socket ${location} -> ${socket}`);
+        }, 5000);
       }, this);
     socket.on('message', function (event) {
       const data = JSON.parse(event.data);
