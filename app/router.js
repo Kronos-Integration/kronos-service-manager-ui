180f5ca927f766fa5898032be9fbc92c5b84db5a
Markus Felten <Markus.Felten@gmx.de>
2015-12-14 13:43:48 +0100

    chore(dependencies): ember-cli@2.2.0-beta.1

--- app/router.js
+++ app/router.js
@@ -1,13 +1,15 @@
 import Ember from 'ember';
 import config from './config/environment';
 
-var Router = Ember.Router.extend({
-    location: config.locationType
+const Router = Ember.Router.extend({
+  location: config.locationType
 });
 
 export default Router.map(function () {
   this.route('flows');
-  this.route('flow', { path: '/flows/:flow_id' });
+  this.route('flow', {
+    path: '/flows/:flow_id'
+  });
 
   this.route('login');
   this.route('state');
