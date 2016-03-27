9b02a0ae58cf7ce8f3fb06cb7955e4a5097a136f
Markus Felten <Markus.Felten@gmx.de>
2016-03-27 20:10:11 +0200

    chore(dependencies): fixes for ember-cli@2.4.3

--- tests/helpers/module-for-acceptance.js
+++ tests/helpers/module-for-acceptance.js
@@ -1,26 +1,23 @@
-import {
-  module
-}
-from 'qunit';
+import { module } from 'qunit';
 import startApp from '../helpers/start-app';
 import destroyApp from '../helpers/destroy-app';
 
-export default function (name, options = {}) {
+export default function(name, options = {}) {
   module(name, {
     beforeEach() {
-        this.application = startApp();
+      this.application = startApp();
 
-        if (options.beforeEach) {
-          options.beforeEach.apply(this, arguments);
-        }
-      },
-
-      afterEach() {
-        if (options.afterEach) {
-          options.afterEach.apply(this, arguments);
-        }
-
-        destroyApp(this.application);
+      if (options.beforeEach) {
+        options.beforeEach.apply(this, arguments);
       }
+    },
+
+    afterEach() {
+      if (options.afterEach) {
+        options.afterEach.apply(this, arguments);
+      }
+
+      destroyApp(this.application);
+    }
   });
 }
