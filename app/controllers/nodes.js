5988522320671a45730fbc5df5ae87ed74c9de3d
Markus Felten <Markus.Felten@gmx.de>
2016-05-08 12:54:24 +0200

    fix(nodes): websocket reconnect

--- app/controllers/nodes.js
+++ app/controllers/nodes.js
@@ -4,33 +4,29 @@ import Util from '../models/Util';
 export default Ember.Controller.extend({
 
   socketService: Ember.inject.service('websockets'),
+
   init() {
     this._super.apply(this, arguments);
 
-    createSocket.call(this, `ws://${window.location.host}/nodes`);
+    const location = `ws://${window.location.host}/nodes`;
 
     let intervalHandler;
+    let socket = this.get('socketService').socketFor(location);
 
-    function createSocket(location) {
-      let socket = this.get('socketService').socketFor(location);
-
-      socket.on('open', () => {
-        socket.send(JSON.stringify({
-          update: true
-        }));
-        clearInterval(intervalHandler);
-      }, this);
-      socket.on('close', () => {
-        console.log('close');
-        socket = undefined;
-        intervalHandler = setInterval(() => {
-          console.log('reopen...');
-          createSocket.call(this, location);
-        }, 5000);
-      }, this);
-      socket.on('message', event => {
-        Util.updateNodes(JSON.parse(event.data));
-      }, this);
-    }
+    socket.on('open', () => {
+      socket.send(JSON.stringify({
+        update: true
+      }));
+      clearInterval(intervalHandler);
+    }, this);
+    socket.on('close', () => {
+      intervalHandler = setInterval(() => {
+        socket.reconnect();
+      }, 5000);
+    }, this);
+    socket.on('message', event => {
+      console.log(`nodes: ${event.data}`);
+      Util.updateNodes(JSON.parse(event.data));
+    }, this);
   }
 });
