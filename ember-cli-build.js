c82b21732c5a7fc988b623057ed95091e8301dea
Markus Felten <Markus.Felten@gmx.de>
2016-07-08 21:42:17 +0200

    refactor: playing around with some babel options

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -2,9 +2,14 @@
 /* global require, module */
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
-module.exports = function(defaults) {
+module.exports = function (defaults) {
   var app = new EmberApp(defaults, {
-    // Add options here
+    babel: {
+      /*
+      includePolyfill: true,
+      experimental: true
+      */
+    }
   });
 
   // Use `app.import` to add additional libraries to the generated
